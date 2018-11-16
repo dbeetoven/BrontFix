084eb158cd17a1ee416a38cf6084a6015b5bbf74
beetoven <beetovenson@gmail.com>
2018-11-16 00:38:50 -0300

    Proyecto refactorizado correctamente. all bugs fixed.

--- src/app/main/utils/error/ierror-handler.ts
+++ src/app/main/utils/error/ierror-handler.ts
@@ -1,5 +1,4 @@
 import { IExeptionArrayItem } from '../exeptions/iexeption-array-item';
-import { Message } from '_debugger';
 export interface IErrorHandler {
     exeptions: IExeptionArrayItem[];
     logExeption(message: string, id?: number): void;
