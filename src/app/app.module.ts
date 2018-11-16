c38436c816a393b1ef8bf44716ba9e6f7d2cfac1
beetoven <beetovenson@gmail.com>
2018-11-16 00:18:08 -0300

    Proyecto actualizado a angular 7 y borrado de cosas unitizable en esta version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,32 @@
-import { HttpClientModule } from "@angular/common/http";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { Angular2FontawesomeModule } from "angular2-fontawesome/angular2-fontawesome";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MainModule } from "./main/main.module";
 import { MainComponent } from "./main/main.component";
-import { AngularFirestore, AngularFirestoreModule } from 'angularfire2/firestore';
-import { CommonModule } from "@angular/common";
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { AngularFireDatabaseModule } from "angularfire2/database";
-import { AngularFireAuthModule } from "angularfire2/auth";
-import { AngularFireModule } from "angularfire2";
+// import { AngularFireDatabaseModule } from "angularfire2/database";
+// import { AngularFireAuthModule } from "angularfire2/auth";
+// import { AngularFireModule } from "angularfire2";
 
-export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyDC3ijkLJzXTLlTLM3nWmqRsYxusjTVgVc",
-    authDomain: "mercadoservicio-a83f6.firebaseapp.com",
-    databaseURL: "https://mercadoservicio-a83f6.firebaseio.com",
-    projectId: "mercadoservicio-a83f6",
-    storageBucket: "mercadoservicio-a83f6.appspot.com",
-    messagingSenderId: "242315262698"
-  }
-};
+// export const environment = {
+//   production: true,
+//   firebase: {
+//     apiKey: "AIzaSyDC3ijkLJzXTLlTLM3nWmqRsYxusjTVgVc",
+//     authDomain: "mercadoservicio-a83f6.firebaseapp.com",
+//     databaseURL: "https://mercadoservicio-a83f6.firebaseio.com",
+//     projectId: "mercadoservicio-a83f6",
+//     storageBucket: "mercadoservicio-a83f6.appspot.com",
+//     messagingSenderId: "242315262698"
+//   }
+// };
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
     BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    MainModule,
-    Angular2FontawesomeModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireAuthModule
+    NgbModule.forRoot(),
+    MainModule
   ],
   providers: [],
   bootstrap: [MainComponent]
 })
-export class AppModule {}
+export class AppModule { }
