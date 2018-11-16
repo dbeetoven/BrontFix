b86d271b46f22ddec878279256a5747ff8b90046
beetoven <beetovenson@gmail.com>
2018-11-16 00:47:35 -0300

    lint rxjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,6 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MainModule } from "./main/main.module";
 import { MainComponent } from "./main/main.component";
-// import { AngularFireDatabaseModule } from "angularfire2/database";
-// import { AngularFireAuthModule } from "angularfire2/auth";
-// import { AngularFireModule } from "angularfire2";
-
-// export const environment = {
-//   production: true,
-//   firebase: {
-//     apiKey: "AIzaSyDC3ijkLJzXTLlTLM3nWmqRsYxusjTVgVc",
-//     authDomain: "mercadoservicio-a83f6.firebaseapp.com",
-//     databaseURL: "https://mercadoservicio-a83f6.firebaseio.com",
-//     projectId: "mercadoservicio-a83f6",
-//     storageBucket: "mercadoservicio-a83f6.appspot.com",
-//     messagingSenderId: "242315262698"
-//   }
-// };
 
 @NgModule({
   declarations: [],
